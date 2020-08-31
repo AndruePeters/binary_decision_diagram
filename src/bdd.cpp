3cc48b82312c7ea16c8f308e0ff6fa9e10b9bc68
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-08-31 16:39:45 -0400

    Removed C BDD code, and have working implementation of BDD

--- src/bdd.cpp
+++ src/bdd.cpp
@@ -1,18 +1,19 @@
 #include "bdd.h"
 
 #include <algorithm>
-#include <unordered_map>
 #include <iostream>
+#include <stack>
+#include <unordered_map>
 
-BinaryDecisionDiagram::Node* BinaryDecisionDiagram::makeNode(std::size_t index, Node* high, Node* low)
+BinaryDecisionDiagram::Node *BinaryDecisionDiagram::makeNode(std::size_t index, Node *high, Node *low)
 {
-    if ( (index != 0  && index != 1) && high == low) {
+    if ((index != 0 && index != 1) && high == low) {
         return high;
     }
     // lambda to actually create the node
     auto addNode = [this, index, high, low]() {
         auto newNode = std::make_unique<BinaryDecisionDiagram::Node>(index, high, low);
-        indexToNode.insert({index, newNode.get()});
+        indexToNode.insert({ index, newNode.get() });
         nodes.push_back(std::move(newNode));
         return nodes.back().get();
     };
@@ -27,10 +28,9 @@ BinaryDecisionDiagram::Node* BinaryDecisionDiagram::makeNode(std::size_t index, 
 
     // attempt to see if the given node parameters already exist
     // this is a node where the index, high, and low are all the same
-    auto nodeExistsIterator = std::find_if(indexRangeBegin, indexRangeEnd,
-        [high, low](const auto& nodeItPtr) {
-            return (nodeItPtr.second->high_ == high) && (nodeItPtr.second->low_ == low);
-        });
+    auto nodeExistsIterator = std::find_if(indexRangeBegin, indexRangeEnd, [high, low](const auto &nodeItPtr) {
+        return (nodeItPtr.second->high_ == high) && (nodeItPtr.second->low_ == low);
+    });
 
     // if the node does not exist then add the node
     if (nodeExistsIterator == indexRangeEnd) {
@@ -41,8 +41,7 @@ BinaryDecisionDiagram::Node* BinaryDecisionDiagram::makeNode(std::size_t index, 
     return nodeExistsIterator->second;
 }
 
-BinaryDecisionDiagram::Node::Node(std::size_t index, Node* high, Node* low):
-    index_(index), high_(high), low_(low)   { }
+BinaryDecisionDiagram::Node::Node(std::size_t index, Node *high, Node *low) : index_(index), high_(high), low_(low) {}
 
 BinaryDecisionDiagram::BinaryDecisionDiagram(std::size_t estimatedNumberVariables)
 {
@@ -54,13 +53,15 @@ BinaryDecisionDiagram::BinaryDecisionDiagram(std::size_t estimatedNumberVariable
     makeNode(1, nullptr, nullptr);
 }
 
-BinaryDecisionDiagram::Node* BinaryDecisionDiagram::addNthIndex(std::size_t n) {
+BinaryDecisionDiagram::Node *BinaryDecisionDiagram::addNthIndex(std::size_t n)
+{
     return makeNode(n, nodes[1].get(), nodes[0].get());
 }
 
 // The if then else operation computers and returns the node that is the result of applying the if-then-else operator
 // to three nodes that serve as the if, then, and else clauses
-BinaryDecisionDiagram::Node* BinaryDecisionDiagram::ifThenElse(BinaryDecisionDiagram::Node* ifNode, BinaryDecisionDiagram::Node* thenNode, BinaryDecisionDiagram::Node* elseNode) {
+BinaryDecisionDiagram::Node *BinaryDecisionDiagram::ifThenElse(BinaryDecisionDiagram::Node *ifNode, BinaryDecisionDiagram::Node *thenNode, BinaryDecisionDiagram::Node *elseNode)
+{
     if (ifNode == one()) { return thenNode; }
     if (ifNode == zero()) { return elseNode; }
     if (thenNode == elseNode) { return thenNode; }
@@ -72,22 +73,21 @@ BinaryDecisionDiagram::Node* BinaryDecisionDiagram::ifThenElse(BinaryDecisionDia
     if (splitVar < thenNode->index_) { splitVar = thenNode->index_; }
     if (splitVar < elseNode->index_) { splitVar = elseNode->index_; }
 
-    Node* ifTrue = restrict(ifNode, splitVar, true);
-    Node* thenTrue = restrict(thenNode, splitVar, true);
-    Node* elseTrue = restrict(elseNode, splitVar, true);
-    Node* positiveFtor = ifThenElse(ifTrue, thenTrue, elseTrue);
+    Node *ifTrue = restrict(ifNode, splitVar, true);
+    Node *thenTrue = restrict(thenNode, splitVar, true);
+    Node *elseTrue = restrict(elseNode, splitVar, true);
+    Node *positiveFtor = ifThenElse(ifTrue, thenTrue, elseTrue);
 
-    Node* ifFalse = restrict(ifNode, splitVar, false);
-    Node* thenFalse = restrict(thenNode, splitVar, false);
-    Node* elseFalse = restrict(elseNode, splitVar, false);
-    Node* negativeFtor = ifThenElse(ifFalse, thenFalse, elseFalse);
+    Node *ifFalse = restrict(ifNode, splitVar, false);
+    Node *thenFalse = restrict(thenNode, splitVar, false);
+    Node *elseFalse = restrict(elseNode, splitVar, false);
+    Node *negativeFtor = ifThenElse(ifFalse, thenFalse, elseFalse);
 
     return makeNode(splitVar, positiveFtor, negativeFtor);
 }
 
-BinaryDecisionDiagram::Node* BinaryDecisionDiagram::restrict(BinaryDecisionDiagram::Node* root, std::size_t index, bool val)
+BinaryDecisionDiagram::Node *BinaryDecisionDiagram::restrict(BinaryDecisionDiagram::Node *root, std::size_t index, bool val)
 {
-    std::cout << "Attempting to add index(" << index << ") with val(" << val << ")\n\n";
     if (root->index_ < index) {
         return root;
     }
@@ -101,9 +101,10 @@ BinaryDecisionDiagram::Node* BinaryDecisionDiagram::restrict(BinaryDecisionDiagr
     return val ? restrict(root->high_, index, val) : restrict(root->low_, index, val);
 }
 
-std::vector<BinaryDecisionDiagram::Node*> BinaryDecisionDiagram::getNodes(std::size_t variableSubscript) {
-    std::vector<BinaryDecisionDiagram::Node*> retNodes;
-    for (const auto& nodePtr: nodes) {
+std::vector<BinaryDecisionDiagram::Node *> BinaryDecisionDiagram::getNodes(std::size_t variableSubscript)
+{
+    std::vector<BinaryDecisionDiagram::Node *> retNodes;
+    for (const auto &nodePtr : nodes) {
         if (nodePtr->index_ == variableSubscript) {
             retNodes.push_back(nodePtr.get());
         }
@@ -111,13 +112,84 @@ std::vector<BinaryDecisionDiagram::Node*> BinaryDecisionDiagram::getNodes(std::s
     return retNodes;
 }
 
+std::string toDot(BinaryDecisionDiagram::Node *root, BinaryDecisionDiagram::Node *one, BinaryDecisionDiagram::Node *zero, const std::string &graphName)
+{
+    // keep track of nodes that have been visited
+    std::unordered_map<BinaryDecisionDiagram::Node *, bool> visited{ { zero, true }, { one, true } };
 
-std::string toDot(BinaryDecisionDiagram& bdd, const std::string& name)
+    // map unique graph id to label
+    std::unordered_map<std::size_t, std::string> idToLabel{ { 0, "0" }, { 1, "1" } };
+
+    // map a node pointer to an id
+    std::unordered_map<BinaryDecisionDiagram::Node *, std::size_t> nodeToId = { { zero, 0 }, { one, 1 } };
+
+    // used for dfs algorithm
+    std::stack<BinaryDecisionDiagram::Node *> nodeStack;
+    nodeStack.push(root);
+
+    std::string dotInner;
+    std::size_t uniqueGraphId = 2;
+    while (!nodeStack.empty()) {
+        auto *node = nodeStack.top();
+        nodeStack.pop();
+
+        // check to see if we've visited
+        if (visited.count(node) != 0) {
+            visited[node] = true;
+            continue;
+        }
+
+        const std::size_t nodeID = nodeToId.count(node) == 0 ? uniqueGraphId : nodeToId[node];
+
+        if (nodeID == uniqueGraphId) {
+            nodeToId[node] = uniqueGraphId;
+            idToLabel[uniqueGraphId++] = std::to_string(node->index_);
+        }
+
+
+        visited[node] = true;
+
+        if (node->high_ != nullptr) {
+            if (nodeToId.count(node->high_) == 0) {
+                nodeToId[node->high_] = uniqueGraphId;
+                idToLabel[uniqueGraphId++] = std::to_string(node->high_->index_);
+            }
+
+            // need to map from graphiz node to graphiz node
+            dotInner += "\t" + std::to_string(nodeID) + " -- " +  std::to_string(nodeToId[node->high_])  + ";\n";
+            if (visited.count(node->high_) == 0) {
+                nodeStack.push(node->high_);
+            }
+        }
+
+        if (node->low_ != nullptr) {
+            if (nodeToId.count(node->low_) == 0) {
+                nodeToId[node->low_] = uniqueGraphId;
+                idToLabel[uniqueGraphId++] = std::to_string(node->low_->index_);
+            }
+
+
+            dotInner += "\t" + std::to_string(nodeID) + " -- " + std::to_string(nodeToId[node->low_]) + " [style=dashed];\n";
+            if (visited.count(node->low_) == 0) {
+                nodeStack.push(node->low_);
+            }
+        }
+    }
+
+    std::string graphLabels;
+    for (auto &[id, label] : idToLabel) {
+        graphLabels += "\t" + std::to_string(id) + " [label=\"" + label + "\"];\n";
+    }
+
+    return "graph " + graphName + " {\n" + graphLabels + dotInner + "}";
+}
+
+std::string toDot(BinaryDecisionDiagram &bdd, const std::string &name)
 {
     std::string dotInner;
     std::size_t graphId = 0;
     std::unordered_map<std::size_t, std::string> idToLabel;
-    for (auto& nodePtr: bdd) {
+    for (auto &nodePtr : bdd) {
         idToLabel[graphId] = std::to_string(nodePtr->index_);
         const std::string graphIdStr = std::to_string(graphId);
         if (nodePtr->low_ != nullptr) {
@@ -132,205 +204,27 @@ std::string toDot(BinaryDecisionDiagram& bdd, const std::string& name)
 
     // build the label string to be at the top
     std::string labelStr;
-    for (auto& [id, label]: idToLabel) {
-      labelStr += std::to_string(id) + " [label=\"" + label + "\"];\n";
+    for (auto &[id, label] : idToLabel) {
+        labelStr += std::to_string(id) + " [label=\"" + label + "\"];\n";
     }
-    return "graph " + name + "{\n" + labelStr +  dotInner + "\n}";
+    return "graph " + name + "{\n" + labelStr + dotInner + "\n}";
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 namespace std {
-template<> struct hash<BinaryDecisionDiagram::Node> {
-    std::size_t operator()(BinaryDecisionDiagram::Node const& node) const
+template<>
+struct hash<BinaryDecisionDiagram::Node>
+{
+    std::size_t operator()(BinaryDecisionDiagram::Node const &node) const
     {
         const std::size_t indexHash = std::hash<std::size_t>{}(node.index_);
-        const std::size_t highHash = std::hash<BinaryDecisionDiagram::Node*>{}(node.high_);
-        const std::size_t lowHash = std::hash<BinaryDecisionDiagram::Node*>{}(node.low_) << 1;
+        const std::size_t highHash = std::hash<BinaryDecisionDiagram::Node *>{}(node.high_);
+        const std::size_t lowHash = std::hash<BinaryDecisionDiagram::Node *>{}(node.low_) << 1;
         const std::size_t lowHighHash = (highHash ^ lowHash) << 1;
         const std::size_t finalHash = (indexHash ^ lowHighHash) << 1;
         return finalHash;
     }
 };
-}
-
-BDD** BDDTable = nullptr;
-// #define PAIR(a ,b) ( ( std::size_t(a)+ std::size_t(b) )∗(std::size_t(a)+std::size_t(b)+(1) ) /(2)+std::size_t(a) )
-// #define TRIPLE(a ,b, c) ((std::size_t int ) (PAIR((std::size_t int )c ,PAIR ((std::size_t )a ,(std::size_t )b) ) ) )
-// #define NODEHASH( lvl , l , h) (TRIPLE( (std::size_t )( lvl ) ,(std::size_t)( l ) ,(std::size_t)(h) ) % 100 ) ;
-
-std::size_t pair_func(const std::size_t a, const std::size_t b)
-{
-    const std::size_t term1 = a + b;
-    const std::size_t term2 = a + b + 1;
-    const std::size_t term3 = 2 + a;
-    return (term1 * term2) / term3;
-}
-
-std::size_t triple_func(std::size_t a, std::size_t b, std::size_t c)
-{
-    const std::size_t pair = pair_func(a,b);
-    return pair_func(c, pair);
-}
-
-std::size_t node_hash(std::size_t lvl, std::size_t l, std::size_t h)
-{
-    const std::size_t nodeSize = 100;
-    return triple_func(lvl, l, h) % nodeSize;
-}
-
-// Used to make sure that a new node is unique
-BDD* makeNode(int var, BDD* high, BDD* low) {
-    const std::size_t hash = node_hash(
-      static_cast<std::size_t>(var), reinterpret_cast<std::size_t>(high),
-      reinterpret_cast<std::size_t>(low));
-
-    if (BDDTable == nullptr) {
-      BDDTable = new BDD* [100];
-      std::cout << "size of BDD*[100]: " << sizeof(BDD*[100]);
-    }
-    // no matching bucket found
-    if (BDDTable[hash] == nullptr ) {
-        BDDTable[hash] = new BDD(var, high, low);
-        return BDDTable[hash];
-    } else {
-        BDD* node = BDDTable[hash]; //< first node in bucket
-
-        // search bucket for the matching node
-        while (node->next && (node->index != var || node->high != high || node->low != low)) {
-            node = node->next;
-        }
-
-        // for the case of no matching node in bucket
-        if (node->index != var || node->high != high || node->low != low) {
-            // make a new node and add to bucket
-            BDD* newNode = new BDD(var, high, low);
-            node->next = newNode;
-            return newNode;
-        } else {
-          return node;
-        }
-    }
-}
-
-BDD* bdd_true()
-{
-  if (BDDTable[1] == nullptr) {
-    BDDTable[1] = makeNode(1, nullptr, nullptr);
-  }
-
-  return BDDTable[1];
-}
-
-BDD* bdd_false()
-{
-  if (BDDTable[0] == nullptr) {
-    BDDTable[0] = makeNode(0, nullptr, nullptr);
-  }
-
-  return BDDTable[0];
-}
-
-BDD* ithvar(int i) {
-    static int numvars = 2;
-
-    if (BDDTable == nullptr) {
-      BDDTable = new BDD* [100];
-      std::cout << "size of BDD*[100]: " << sizeof(BDD*[100]);
-    }
-
-    BDDTable[0]->index = numvars;
-    BDDTable[1]->index = numvars;
-    ++numvars;
-    return makeNode(i, bdd_true(), bdd_false());
-}
-
-/// Computes a BDD that results from assigning all isntances of the variable var witht he truth value val.
-BDD* restrict(BDD* subtree, int var, bool val) {
-    if (subtree->index > var) {
-        return subtree;
-    } else if (subtree->index < var) {
-        return makeNode(subtree->index, restrict(subtree->high, var, val), restrict(subtree->low, var, val));
-    } else { // subtree->index == var
-        if (val) {
-            return restrict(subtree->high, var, val);
-        } else {
-            return restrict(subtree->low, var, val);
-        }
-    }
-}
-
-/// The ITE (if-then-else) operation computes and returns the BDD that is the result of applying the if-then-else
-/// operator to three BDDs that serve as teh if, then, and else clauses
-BDD* ite(BDD* I, BDD* T, BDD* E) {
-    // Base cases
-    if (I == bdd_true())    return T;
-    if (I == bdd_false())   return E;
-    if (T == E)             return T;
-    if (T == bdd_true() && E == bdd_false()) return I;
-
-    // General cases
-    // splitting variable must be the topmost root
-    int splitVar = I->index;
-    if (splitVar > T->index) { splitVar = T->index; }
-    if (splitVar > E->index) { splitVar = E->index; }
-
-    BDD* Ixt = restrict(I, splitVar, true);
-    BDD* Txt = restrict(T, splitVar, true);
-    BDD* Ext = restrict(E, splitVar, true);
-    BDD* posFtor = ite(Ixt, Txt, Ext);
-
-    BDD* Ixf = restrict(I, splitVar, false);
-    BDD* Txf = restrict(T, splitVar, false);
-    BDD* Exf = restrict(E, splitVar, false);
-    BDD* negFtor = ite(Ixf, Txf, Exf);
-
-    BDD* result = makeNode(splitVar, posFtor, negFtor);
-    return result;
-}
-
-BDD* bdd_and(BDD* lhs, BDD* rhs) {
-    return ite(lhs, rhs, bdd_false());
-}
-
-double satcount_rec(BDD* subtree) {
-    if (subtree == bdd_false() ) {
-        return 0;
-    } else if (subtree == bdd_true()) {
-        return 1;
-    }
-
-    BDD* low = subtree->low;
-    BDD* high = subtree->high;
-    double s = std::pow(2, low->index - subtree->index - 1);
-    double size = s * satcount_rec(low);
-    s = std::pow(2, high->index - subtree->index - 1);
-    size += s * satcount_rec(high);
-    return size;
-}
-
-double satcount(BDD* subtree) {
-    return std::pow(2, (subtree->index - 1) * satcount_rec(subtree));
-}
-
-BDD* satone_rec(BDD* subtree) {
-    assert(subtree != 0);
-
-    if (subtree == bdd_false() || subtree == bdd_true()) {
-        return subtree;
-    } else if (subtree->low == bdd_false()) {
-        return makeNode(subtree->index, bdd_false(), satone_rec(subtree->high));
-    } else { // subtree->high == bdd_false()
-        return makeNode(subtree->index, satone_rec(subtree->low), bdd_false());
-    }
-}
-
-BDD* satone(BDD* subtree) {
-    assert(subtree != 0);
-    if (subtree == bdd_false()) {
-        return 0;
-    }
-
-    return satone_rec(subtree);
-}
+}// namespace std
 
--- src/bdd.h
+++ src/bdd.h
@@ -35,65 +35,21 @@ public:
     Node* addNthIndex(std::size_t n);
     Node* ifThenElse(Node* ifNode, Node* thenNode, Node* elseNode);
     Node* restrict(Node* root, std::size_t var, bool val);
-    std::vector<Node*> getNodes(std::size_t variableSubscript = 0);
+    std::vector<Node*> getNodes(std::size_t variableSubscript);
 
     Node* andOperation(Node* lhs, Node* rhs) { return ifThenElse(lhs, rhs, zero()); }
+    Node* orOperation(Node* lhs, Node* rhs) { return ifThenElse(lhs, one(), rhs);}
+    Node* xorOperation(Node* lhs, Node* rhs) { return ifThenElse(lhs, ifThenElse(rhs, zero(), one()), rhs); }
+    std::vector<std::unique_ptr<Node>>& getNodes() { return nodes; }
 
     auto begin() { return nodes.begin(); }
     auto end() { return nodes.end(); }
 private:
     std::vector<std::unique_ptr<Node>> nodes;
     std::unordered_multimap<std::size_t, Node*> indexToNode;
-
-    Node* ifThenElse();
 };
 
 std::string toDot(BinaryDecisionDiagram& bdd, const std::string& name);
+std::string toDot(BinaryDecisionDiagram::Node* root, BinaryDecisionDiagram::Node* one, BinaryDecisionDiagram::Node* zero, const std::string& graphName);
 
-/// Represents a Binary Decision Diagram
-/// Every node can be thought of as its own BDD
-/// Comes from realization that every boolean function can be written in "IF THEN ELSE" (ITE) form
-struct BDD {
-  BDD(int index_, BDD* high_, BDD* low_) : index(index_), high(high_), low(low_) {}
-  int index = 0;      ///<    subscript in the variable ordering, so lower means closer to the top
-  BDD* high = nullptr;      ///<    pointer to the "THEN" bdd
-  BDD* low = nullptr;       ///<    pointer to the "ELSE" bdd
-  BDD* next = nullptr;      ///<    pointer to the next bdd in the bucket
-};
-
-// #define PAIR(a ,b) ( ( std::size_t(a)+ std::size_t(b) )∗(std::size_t(a)+std::size_t(b)+(1) ) /(2)+std::size_t(a) )
-// #define TRIPLE(a ,b, c) ((std::size_t int ) (PAIR((std::size_t int )c ,PAIR ((std::size_t )a ,(std::size_t )b) ) ) )
-// #define NODEHASH( lvl , l , h) (TRIPLE( (std::size_t )( lvl ) ,(std::size_t)( l ) ,(std::size_t)(h) ) % 100 ) ;
-
-std::size_t pair_func(const std::size_t a, const std::size_t b);
-std::size_t triple_func(std::size_t a, std::size_t b, std::size_t c);
-
-std::size_t node_hash(std::size_t lvl, std::size_t l, std::size_t h);
-
-
-
-// Used to make sure that a new node is unique
-BDD* makeNode(int var, BDD* high, BDD* low);
-
-BDD* bdd_true();
-
-BDD* bdd_false();
-
-BDD* ithvar(int i);
-
-/// Computes a BDD that results from assigning all isntances of the variable var witht he truth value val.
-BDD* restrict(BDD* subtree, int var, bool val);
-
-/// The ITE (if-then-else) operation computes and returns the BDD that is the result of applying the if-then-else
-/// operator to three BDDs that serve as teh if, then, and else clauses
-BDD* ite(BDD* I, BDD* T, BDD* E);
-
-BDD* bdd_and(BDD* lhs, BDD* rhs);
-double satcount_rec(BDD* subtree);
-
-double satcount(BDD* subtree);
-
-BDD* satone_rec(BDD* subtree);
-
-BDD* satone(BDD* subtree);
 #endif
