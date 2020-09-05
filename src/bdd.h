4b17ff5d77d16f628439ae5615d7e11edc94eb2c
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2020-09-04 23:49:51 -0400

    updated formatting and added complement, nor, equivalence, nand, and inhibition operations

--- src/bdd.h
+++ src/bdd.h
@@ -13,27 +13,25 @@
 #include <memory>
 #include <unordered_map>
 
-struct BinaryDecisionDiagram {
+struct BinaryDecisionDiagram
+{
     BinaryDecisionDiagram(std::size_t index, BinaryDecisionDiagram* high, BinaryDecisionDiagram* low);
     std::size_t index_ = 0;
     BinaryDecisionDiagram* high_ = nullptr;
     BinaryDecisionDiagram* low_ = nullptr;
 };
 
-
 /// Binary Decision Diagram
 /// Each node contains the index (subscript) and a pointer to the next node
 class BinaryDecisionDiagramManager
 {
-public:
-
-
+  public:
     BinaryDecisionDiagram* makeBinaryDecisionDiagram(std::size_t index, BinaryDecisionDiagram* high, BinaryDecisionDiagram* low);
 
     explicit BinaryDecisionDiagramManager(std::size_t estimatedNumberVariables = 100);
 
     // Returns a constant references to the zero/one BinaryDecisionDiagram
-    BinaryDecisionDiagram* one() const { return  nodes[1].get(); }
+    BinaryDecisionDiagram* one() const { return nodes[1].get(); }
     BinaryDecisionDiagram* zero() const { return nodes[0].get(); }
 
     BinaryDecisionDiagram* addNthIndex(std::size_t n);
@@ -42,19 +40,23 @@ public:
     std::vector<BinaryDecisionDiagram*> getNodes(std::size_t variableSubscript);
 
     BinaryDecisionDiagram* andOperation(BinaryDecisionDiagram* lhs, BinaryDecisionDiagram* rhs) { return ifThenElse(lhs, rhs, zero()); }
-    BinaryDecisionDiagram* orOperation(BinaryDecisionDiagram* lhs, BinaryDecisionDiagram* rhs) { return ifThenElse(lhs, one(), rhs);}
-    BinaryDecisionDiagram* xorOperation(BinaryDecisionDiagram* lhs, BinaryDecisionDiagram* rhs) { return ifThenElse(lhs, negate(rhs), rhs); }
-    BinaryDecisionDiagram* negate(BinaryDecisionDiagram* node) { return ifThenElse(node, zero(), one()); }
-    std::vector<std::unique_ptr<BinaryDecisionDiagram>>& getNodes() { return nodes; }
+    BinaryDecisionDiagram* orOperation(BinaryDecisionDiagram* lhs, BinaryDecisionDiagram* rhs) { return ifThenElse(lhs, one(), rhs); }
+    BinaryDecisionDiagram* xorOperation(BinaryDecisionDiagram* lhs, BinaryDecisionDiagram* rhs) { return ifThenElse(lhs, complement(rhs), rhs); }
+    BinaryDecisionDiagram* complement(BinaryDecisionDiagram* node) { return ifThenElse(node, zero(), one()); }
+    BinaryDecisionDiagram* norOperation(BinaryDecisionDiagram* lhs, BinaryDecisionDiagram* rhs) { return ifThenElse(lhs, zero(), complement(rhs)); }
+    BinaryDecisionDiagram* equivalence(BinaryDecisionDiagram* lhs, BinaryDecisionDiagram* rhs) { return ifThenElse(lhs, rhs, complement(rhs)); }
+    BinaryDecisionDiagram* nandOperation(BinaryDecisionDiagram* lhs, BinaryDecisionDiagram* rhs) { return ifThenElse(lhs, complement(rhs), zero()); }
+    BinaryDecisionDiagram* inhibition(BinaryDecisionDiagram* lhs, BinaryDecisionDiagram* rhs) { return ifThenElse(lhs, complement(rhs), zero()); }
 
     auto begin() { return nodes.begin(); }
     auto end() { return nodes.end(); }
-private:
+
+  private:
     std::vector<std::unique_ptr<BinaryDecisionDiagram>> nodes;
     std::unordered_multimap<std::size_t, BinaryDecisionDiagram*> indexToNode;
 };
 
-std::string toDot(BinaryDecisionDiagramManager & bdd, const std::string& name);
+std::string toDot(BinaryDecisionDiagramManager& bdd, const std::string& name);
 std::string toDot(BinaryDecisionDiagram* root, BinaryDecisionDiagram* one, BinaryDecisionDiagram* zero, const std::string& graphName);
 
 #endif
